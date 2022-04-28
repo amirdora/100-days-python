a3a55265dfc572ffc72e7a81de00efbce9ee1cfd
ad <amirdora@gmail.com>
2022-04-28 21:38:19 +0200

    day2

---
+++ Day02/day02-ex1.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# type conversion 
+
+digitOne = int(two_digit_number[0])
+digitTwo = int(two_digit_number[1])
+
+sumDigits = digitOne + digitTwo
+
+print(str(sumDigits))
