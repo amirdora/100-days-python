ea09103d3b10625f722cbc688683959d88edc042
ad <amirdora@gmail.com>
2022-05-06 22:05:52 +0200

    day 5, loop and range

---
+++ Day05/day05-ex1.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+total =0
+for n in range(0, len(student_heights)):
+    total +=  student_heights[n]
+average = total / len(student_heights)
+
+print(round(average))
+
---
+++ Day05/day05-ex3.py
@@ -0,0 +1,7 @@
+#Write your code below this row 👇
+
+evenSum =0 
+for n in range(0, 101, 2):
+    evenSum += n
+
+print(evenSum)
